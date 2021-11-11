70a7c7f9917cf125019414b74eb0f86edde0cdf0
Ronin <rlombardino44@gmail.com>
2021-11-10 16:37:43 -0800

    Finished functional shaired

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,46 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var Instance = {};
+
+  Instance.storage = {};
+
+  _.extend(Instance, queueMethods);
+
+  return Instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  var HighKey = 0;
+  for (var key in this.storage) {
+    if (key > HighKey) {
+      HighKey = key;
+    }
+  }
+  HighKey += 1;
+  this.storage[HighKey] = value;
+};
+
+queueMethods.dequeue = function() {
+  var LowKey = -1;
+  for (var key in this.storage) {
+    if (LowKey === -1 || key < LowKey) {
+      LowKey = key;
+    }
+  }
+  let tempValue = this.storage[LowKey];
+  delete this.storage[LowKey];
+  return tempValue;
+};
+
+queueMethods.size = function() {
+  var size = 0;
+  for (var key in this.storage) {
+    size += 1;
+  }
+  return size;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,43 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  var Instance = {};
 
+  Instance.storage = {};
+
+  _.extend(Instance, stackMethods);
+
+  return Instance;
+};
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  let CurrentIndex = 0;
+  for (var key in this.storage) {
+    if (key > CurrentIndex) {
+      CurrentIndex = key;
+    }
+  }
+  CurrentIndex += 1;
+  this.storage[CurrentIndex] = value;
+};
 
+stackMethods.pop = function() {
+  let CurrentIndex = -1;
+  for (var key in this.storage) {
+    if (CurrentIndex === -1 || key > CurrentIndex) {
+      CurrentIndex = key;
+    }
+  }
+  let TempValue = this.storage[CurrentIndex];
+  delete this.storage[CurrentIndex];
+  return TempValue;
+};
+
+stackMethods.size = function() {
+  var size = 0;
+  for (var key in this.storage) {
+    size++;
+  }
+  return size;
+};
