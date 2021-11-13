78db3853b5e8b70991d90baa2a51563d9e628464
Ronin <rlombardino44@gmail.com>
2021-11-13 13:51:24 -0800

    removed debugger break

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -76,7 +76,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) { //debugger;
+Graph.prototype.forEachNode = function(cb) {
   for (var key in this) {
     if (this[key].value) {
       cb(key);
