0a07f19f69b59a0079fe79d7667a96300b0179db
Ronin <rlombardino44@gmail.com>
2021-11-12 23:12:31 -0800

    Finished Linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (list.head === null) {
+      list.head = newTail;
+    } else {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
+
   };
 
   list.removeHead = function() {
+    if (list.head === null) { /* if (!list.head)*/
+      return null;
+    }
+    var firstHead = list.head;
+    list.head = firstHead.next;
+    return firstHead.value;
   };
 
   list.contains = function(target) {
+    // var contain = function(target, linkList) {
+    //   if (linkList === null) {
+    //     return false;
+    //   }
+    //   if (linkList.value === target) {
+    //     return true;
+    //   }
+    //   if (linkList.next === null) {
+    //     return false;
+    //   }
+    //   return contain(target, linkList.next);
+    // };
+    // return contain(target, list.head);
+
+
+    var CurrentNode = list.head;
+    while (CurrentNode) {
+      if (CurrentNode.value === target) {
+        return true;
+      }
+      CurrentNode = CurrentNode.next;
+    }
+    return false;
+
   };
 
   return list;
@@ -27,3 +65,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
