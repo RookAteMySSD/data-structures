2065b90c0839e4233e6616a9812083f801df9edf
Ronin <rlombardino44@gmail.com>
2021-11-13 11:15:45 -0800

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,71 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
+
+
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create new node by invoking Tree constructor
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  // if (this.value === target) {
+  //   return true;
+  // }
+  // while (this.children.length > 0) {
+  //   if (this.value === target) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  // check root node value is target
+  if (this.value === target) {
+    // return true
+    return true;
+  }
+
+  // check if children is not empty
+  // iterate over children array
+  for (let x = 0; x < this.children.length; x++) {
+    // if children value contains target
+    var child = this.children[x];
+    if (child.contains(target)) {
+      //return true
+      return true;
+    }
+  }
+  // return false
+  return false;
+
+
+
+
+
+
+  // return this.children.contains();
+
+  // var containCheck = function(list) {
+  //   // debugger;
+  //   for (var key in list) {
+  //     if (list[x].value === target) {
+  //       return true;
+  //     }
+  //     if (typeof(list[x].children) === 'object') {
+  //       return containCheck(list[x].children);
+  //     }
+  //   }
+  //   return false;
+  // };
+  // return containCheck(this);
 };
 
 
